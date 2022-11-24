bf3527214f7ee49116115422dd00f720562b1b9a
AntonioSouls <tonyplanza1@gimail.com>
2022-11-24 13:15:07 +0100

    I've created the method to make a diff and started to create the method to save changes

--- memory_diff/diff.py
+++ memory_diff/diff.py
@@ -3,20 +3,38 @@ from difflib import ndiff
 # Diff class that models an object that allows you to make the difference between two files and save the difference to another file
 
 class Diff:
-    # COSTRUTTORE DELLA CLASSE: stiamo dicendo che l'ogetto prende come parametri due file, dove il primo che inseriamo è quello che va a "Destra", il secondo va a "Sinistra"
-    # Uso la convenzione che quello di destra è quello con cui fare il confronto, dunque:
+    ## ---- DA TRADURRE IN INGLESE QUESTA DESCRIZIONE ----------
+    # COSTRUTTORE DELLA CLASSE: stiamo dicendo che l'ogetto prende come parametri due file, dove il primo che inseriamo è quello "Nuovo", il secondo è il "Vecchio"
+    # Uso la convenzione che quello nuovo è quello con cui fare il confronto, dunque:
     #       - se la diff mi ritorna ' ' vuol dire che la riga analizzata è uguale nei due file; 
-    #       - se ritorna '+' la linea esiste solo nel file di destra (cioè è stata aggiunta e la dovremo importare anche in quello di sinistra);
-    #       - se ritorna '-' la linea esiste solo nel file di sinistra (cioè è stata rimossa e la dovremo rimuovere dal file di sinistra);
-    #       - se ritorna ('-', '?', '+') vuol dire che la linea analizzata possiede delle differenze nel mezzo, dunque dovremo prendere quella linea dal file di destra e salvarla uguale nel file di sinistra
+    #       - se ritorna '+' la linea esiste solo nel file che passiamo a destra nella diff (che a noi sarà quello vecchio). Dunque, va rimossa da lì;
+    #       - se ritorna '-' la linea esiste solo nel file che passiamo a sinistra nella diff (che a noi sarà quello nuovo). Dunque, va aggiunta nel file di destra (quello vecchio);
+    #       - se ritorna ('-', '?', '+') vuol dire che la linea analizzata possiede delle differenze nel mezzo, dunque dovremo prendere quella linea dal file nuovo e salvarla in quello vecchio
+    #         rimuovendo quella che già c'era
     
-    def __init__(self,file1, file2) -> None: 
-        self.fileDx = file1
-        self.fileSx = file2
+    def __init__(self,file_new: str, file_old:str) -> None: 
+        self.file_new = file_new
+        self.file_old = file_old
+        self.lista_file_new = None
+        self.lista_file_old = None
 
-    def diff_function(self):
-        diff_result = list(ndiff(self.fileSx.splitlines(),self.fileDx.splitlines()))
+    def diff_function(self):                                      # Fa' semplicemente la diff senza apportare modifiche
+        object_file_new = open(self.file_new, 'r')
+        object_file_old = open(self.file_old, 'r')
+        self.lista_file_new = object_file_new.readlines()
+        self.lista_file_old = object_file_old.readlines()
+        diff_result = list(ndiff(self.lista_file_new.splitlines(),self.lista_file_old.splitlines()))
         return diff_result
 
-    def saving_diff(self):
-        pass 
+    def saving_diff(self):                                      #Fa' la diff invocando la funzione di sopra, e poi sfrutta questa diff per modificare i file
+        diff_result = self.diff_function()
+        for x in diff_result:
+            if x != ' ':
+                if x == '+':
+                    print("Fai qualcosa")
+                else:
+                    if x == '-':
+                        print("Fai altro")
+                    else:
+                        print("Fai altro ancora")
+        return None
