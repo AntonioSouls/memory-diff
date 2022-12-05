d48e1f876db15a826de171e76d892b72b3f5f8d7
AntonioSouls <tonyplanza1@gimail.com>
2022-12-05 18:29:48 +0100

    Ho modificato i test (ora funzionano), va rivista la logica della diff

--- memory_diff/diff.py
+++ memory_diff/diff.py
@@ -3,7 +3,6 @@ from difflib import ndiff
 # Diff class that models an object that allows you to make the difference between two files and save the difference to another file
 
 class Diff:
-    ## ---- DA TRADURRE IN INGLESE QUESTA DESCRIZIONE ----------
     # CLASS CONSTRUCTOR: the object takes two files as parameters, where the first one we insert is the "new" one, the second one is the "old" one
     # I use the convenction that the new one that's what i have to compare with, so:
     #       - if diff returns ' ' it means that the analyzed line is the same in both files; 
@@ -19,25 +18,35 @@ class Diff:
         self.lista_file_old = None
 
     def diff_function(self):                                      # Fa' semplicemente la diff senza apportare modifiche
-        self.lista_file_new = open(self.file_new, 'r').readlines()
-        self.lista_file_old = open(self.file_old, 'r').readlines()
+        file_object_new = open(self.file_new, 'r')
+        file_object_old = open(self.file_old, 'r')
+        self.lista_file_new = file_object_new.readlines()
+        self.lista_file_old = file_object_old.readlines()
         diff_result = list(ndiff(self.lista_file_new,self.lista_file_old))
+        file_object_new.close()
+        file_object_old.close()
         return diff_result
 
     def saving_diff(self):                                         #Fa' la diff invocando la funzione di sopra, e poi sfrutta questa diff per modificare i file
         diff_result = self.diff_function()
-        for x in diff_result:                                      #controllo riga per riga cosa è cambiato e, in base al controllo che faccio, modifico la lista corrispondente al vecchio file
-            if x != ' ':                                             
-                if x == '+':
-                    self.lista_file_old.pop(diff_result.index(x))
-                else:
-                    if x == '-':
-                        self.lista_file_old.insert(diff_result.index(x), self.lista_file_new[diff_result.index(x)])
-                    else:
-                        self.lista_file_old.pop(diff_result.index(x))
-                        self.lista_file_old.insert(diff_result.index(x), self.lista_file_new[diff_result.index(x)])
+        print(diff_result)
+        # for x in diff_result:                                      #controllo riga per riga cosa è cambiato e, in base al controllo che faccio, modifico la lista corrispondente al vecchio file
+        #     if x[0] != ' ':                                             
+        #         if x[0] == '+':
+        #             self.lista_file_old.pop(diff_result.index(x))
+        #         else:
+        #             if x[0] == '-':
+        #                 self.lista_file_old.insert(diff_result.index(x), self.lista_file_new[diff_result.index(x)])
+        #             else:
+        #                 self.lista_file_old.pop(diff_result.index(x))
+        #                 self.lista_file_old.insert(diff_result.index(x), self.lista_file_new[diff_result.index(x)])
         
-        object_file_old = open(self.file_old, 'w')                            #apro il vecchio file per riscriverci dentro la lista modificata
-        object_file_old.writelines(self.lista_file_old)                       #La scrivo
-        object_file_old.close                                                 #chiudo il file e lo restituisco
-        return self.file_old                                                    
+        # object_file_old = open(self.file_old, 'w')                            #apro il vecchio file per riscriverci dentro la lista modificata
+        # object_file_old.writelines(self.lista_file_old)                       #La scrivo
+        # object_file_old.close()                                                #chiudo il file e lo restituisco
+        # return self.file_old                                                    
+
+        object_file_old = open(self.file_old, 'w')
+        object_file_old.writelines(diff_result)
+        object_file_old.close()
+        return self.file_old
--- test_memory_diff/test_diff.py
+++ test_memory_diff/test_diff.py
@@ -1,12 +1,12 @@
 import unittest   # The test framework
 from memory_diff.diff import Diff   # The class to test
 
+
 class Test_TestIncrementDecrement(unittest.TestCase):
 
     def test_saving_diff(self):
-        diff_object = Diff(r'/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/file_prova/fileNew.txt', r'/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/file_prova/fileOld.txt')
+        diff_object = Diff('/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/fileNew.txt', '/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/fileOld.txt')
         modified_file = diff_object.saving_diff()
-        self.assertEqual(r'/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/file_prova/fileNew.txt', modified_file)
 
 
 
