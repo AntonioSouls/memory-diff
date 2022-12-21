e4eee4ace171c95727c185c6d911138897c8afa8
AntonioSouls <tonyplanza1@gimail.com>
2022-12-21 13:41:18 +0100

    Added a method that saves the added/modified TranslatedUnit object into a diff_file.tmx

--- memory_diff/diff.py
+++ memory_diff/diff.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 from memory_diff.components.tu import TranslatedUnit
 from memory_diff.components.tuv import Tuv
 from memory_diff.components.prop import Prop
@@ -14,7 +15,6 @@ class Diff:
         self.file_diff = file_diff
         self.opened_file_new = None
         self.opened_file_old = None
-        self.list_file_diff = []
 
     # Doing diff between two files: an Old File and a New File and saving into a third file only the differences between the preavious files
     def diff_function(self):
@@ -31,10 +31,11 @@ class Diff:
         old_list_tag_tu = Bs_data_old.find_all('tu')
 
 
-        # For each tag <tu> of the new list, create an object TranslatedUnit (using the self.build_object_tu()) and compare that object with all the TranslatedUnit
-        # of the old list, and, if there is not a match, add the object into another list that contains only the added/modified elements
+            ## For each tag <tu> of the new list, create an object TranslatedUnit (using the self.build_object_tu()) and compare that object with all the TranslatedUnit
+            ## of the old list, and, if there is not a match, add the object into another list that contains only the added/modified elements
+        list_block_diff=[]
         for tu_tag in new_list_tag_tu:                                        # Begins loop
-            tu_object_new = TranslatedUnit(tu_tag)                      # Creates the object to compare
+            tu_object_new = TranslatedUnit(tu_tag)                            # Creates the object to compare
             exist:bool = False 
             for i in range(len(old_list_tag_tu)):                             # Does the comparing
                 if exist:
@@ -43,10 +44,52 @@ class Diff:
                 if tu_object_new == tu_object_old:
                     exist = True
             if not exist:
-                self.list_file_diff.append(tu_object_new)
-
-        # Saving the differences into a third file called diff_file
+                list_block_diff.append(tu_object_new)
         
-        ## file_object_diff = open(self.file_diff, 'w')
-        ## file_object_diff.writelines(self.list_file_diff)
-        ## file_object_diff.close() 
+        self.saving_diff(list_block_diff)                                # Invoke saving_diff method to save the diff block into a file
+
+    
+    
+    # Saving the differences into a third file called diff_file
+    def saving_diff(self,list_block_diff):
+        root = ET.Element('tmx')                                        # Root Creation
+        root.set('version','1.4')                                       # Setting root's attributes
+        
+        header = ET.SubElement(root,'header')                           # Header Creation as root's subelement 
+        header.set('creationtool','MyMemory - Database export tool')    # Setting header's attributes
+        header.set('creationtoolversion','1.0')
+        header.set('datatype','plaintext')
+        header.set('o-tmf','MyMemory')
+        header.set('segtype','sentence')
+        header.set('adminlang','en-US')
+        header.set('srclang','en-GB')
+
+        body = ET.SubElement(header,'body')                             # Body creation as header's subelement
+
+        for tu_object in list_block_diff:
+            tu = ET.SubElement(body, 'tu')
+            tu.set('tuid',str(tu_object.getId()))
+            tu.set('srclang',tu_object.get_srclang())
+            tu.set('datatype',tu_object.get_datatype())
+            tu.set('creationdate',tu_object.get_creationdate())
+            tu.set('changedate',tu_object.get_changedate())
+            prop_first = ET.SubElement(tu,'prop')
+            prop_second = ET.SubElement(tu,'prop')
+            tuv_first = ET.SubElement(tu,'tuv')
+            tuv_second = ET.SubElement(tu,'tuv')
+            prop_first.set('type',tu_object.get_prop_first().getType())
+            prop_first.text = tu_object.get_prop_first().getContent()
+            prop_second.set('type',tu_object.get_prop_second().getType())
+            prop_second.text = tu_object.get_prop_second().getContent()
+            tuv_first.set('xml:lang',tu_object.get_tuv_first().get_xml())
+            seg_first = ET.SubElement(tuv_first, 'seg')
+            seg_first.text = tu_object.get_tuv_first().getContent()
+            tuv_second.set('xml:lang',tu_object.get_tuv_second().get_xml())
+            seg_second = ET.SubElement(tuv_second, 'seg')
+            seg_second.text = tu_object.get_tuv_second().getContent()
+        
+        diff_tmx = ET.tostring(root)
+
+        with open(self.file_diff, 'wb') as f:
+            f.write('<?xml version="1.0" encoding="UTF-8"?>')
+            f.write(diff_tmx)
