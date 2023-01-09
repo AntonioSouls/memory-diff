c20bd0b3e5a44cc62e4900425508462f355b4507
AntonioSouls <tonyplanza1@gmail.com>
2023-01-09 13:27:12 +0000

    Started to programm diff for it to work on an entire dataset and not only on two single files

--- test_memory_diff/test_diff.py
+++ test_memory_diff/test_diff.py
@@ -1,15 +1,38 @@
 import unittest   # The test framework
 from memory_diff.diff import Diff   # The class to test
+import os
+import filecmp
 
 
 class Test_TestIncrementDecrement(unittest.TestCase):
 
-    def test_saving_diff(self,file_new: str = '/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/Matecat_New.tmx', file_old:str ='/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/Matecat_Old.tmx', file_diff:str = '/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/diff_file.tmx'):
-        diff_object = Diff(file_new,file_old, file_diff)
-        diff_object.diff_function()
+    # def test_saving_diff(self,file_new: str = '/home/antonio-lanza/dataset/2022.3.0', file_old:str ='/home/antonio-lanza/dataset/2022.2.0', file_diff:str = '/home/antonio-lanza/dataset/diff_file.tmx'):
+    #     diff_object = Diff(file_new,file_old, file_diff)
+    #     diff_object.diff_function()
+    #     return
+
+    def test_diff_on_dataset(self,directory_new: str = '/home/antonio-lanza/dataset/2022.3.0/en__sw', directory_old: str = '/home/antonio-lanza/dataset/2022.2.0/en__sw', directory_diff:str = '/home/antonio-lanza/dataset/diff_folder'):
+        os.chdir(directory_new)
+        list_file_new = os.listdir()
+        os.chdir(directory_old)
+        list_file_old = os.listdir()
+        for file_new in list_file_new:
+            file_old = None
+            for i in range(len(list_file_old)):
+                if filecmp.cmp(file_new,list_file_old[i]):
+                    file_old = list_file_old[i]
+                    break
+            os.chdir(directory_diff)
+            file_diff = os.mkdir(file_new)
+            if file_old == None:
+                with open(file_new,'r') as file_object_new:
+                    opened_file_new = file_object_new.read()
+                with open(file_diff,'wb') as file_object_diff:
+                    file_object_diff.writelines(opened_file_new)
+            else:
+                diff_object = Diff(file_new,file_old,file_diff)
+                diff_object.diff_function()
         return
-
-
 
 
 if __name__ == '__main__':
