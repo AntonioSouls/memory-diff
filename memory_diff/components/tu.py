a92673413b5f70a9dab921c915dd9a89dd2a01e4
Antonio Lanza <92631051+AntonioSouls@users.noreply.github.com>
2023-01-13 10:56:04 +0100

    Added first remove logic (#3)
    * Added first remove logic
    
    * Started to programm diff for it to work on an entire dataset and not only on two single files
    
    * Create test function to test diff on entire dataset - Works but need some corrections
    
    * diff works correctly
    
    Co-authored-by: AntonioSouls <tonyplanza1@gimail.com>

--- memory_diff/components/tu.py
+++ memory_diff/components/tu.py
@@ -10,10 +10,11 @@ class TranslatedUnit:
         tuv_tag_list = tu_tag.find_all('tuv')
         prop_tag_list = tu_tag.find_all('prop')
         self.tuid = int(tu_tag['tuid'])
-        self.srclang = tu_tag['srclang']
-        self.datatype = tu_tag['datatype']
-        self.creationdate = tu_tag['creationdate']
-        self.changedate = tu_tag['changedate']
+        self.srclang = tu_tag['srclang'] if 'srclang' in tu_tag else None
+        self.datatype = tu_tag['datatype'] if 'datatype' in tu_tag else None
+        self.creationdate = tu_tag['creationdate'] if 'creationdate' in tu_tag else None
+        self.changedate = tu_tag['changedate'] if 'changedate' in tu_tag else None
+        self.removed = None
         self.tuv_first = Tuv(tuv_tag_list[0])
         self.tuv_second = Tuv(tuv_tag_list[1])
 
@@ -31,6 +32,11 @@ class TranslatedUnit:
         return self.tuid
     def setId(self, id:int):
         self.tuid = id
+    
+    def getRemoved(self):
+        return self.removed
+    def setRemoved(self, removed:bool):
+        self.removed = removed
     
     def get_srclang(self):
         return self.srclang
--- test_memory_diff/test_diff.py
+++ test_memory_diff/test_diff.py
@@ -1,15 +1,15 @@
 import unittest   # The test framework
 from memory_diff.diff import Diff   # The class to test
-
+from pathlib import Path
+import shutil
+from tqdm import tqdm
 
 class Test_TestIncrementDecrement(unittest.TestCase):
 
-    def test_saving_diff(self,file_new: str = '/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/Matecat_New.tmx', file_old:str ='/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/Matecat_Old.tmx', file_diff:str = '/mnt/c/Users/Antonio/Desktop/memory-diff/test_memory_diff/diff_file.tmx'):
+    def test_saving_diff(self,file_new: str = '/home/antonio-lanza/dataset/2022.3.0/en__sw/Matecat.P60168J72993.en-GB__sw-SZ.tmx', file_old:str ='/home/antonio-lanza/dataset/2022.2.0/en__sw/Matecat.P60168J72993.en-GB__sw-SZ.tmx', file_diff:str = '/home/antonio-lanza/dataset/diff_file.tmx'):
         diff_object = Diff(file_new,file_old, file_diff)
         diff_object.diff_function()
         return
-
-
 
 
 if __name__ == '__main__':
