ffef23f304798020aa4baac6ab163b8639c97fe3
AntonioSouls <tonyplanza1@gmail.com>
2023-01-17 12:33:40 +0000

    Benchmark completed

--- memory_diff/dataset_diff.py
+++ memory_diff/dataset_diff.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from memory_diff.diff import Diff
 import time
 import psutil
+from threading import Thread
 from numpy import mean
 
 # Dataset_diff class that models an object that allows you to make the diff on entire dataset
@@ -11,26 +12,29 @@ from numpy import mean
 class DatasetDiff:
     # CLASS CONSTRUCTOR: the object takes three directory as parameters, where the first one we insert is the dataset new's directory, the second one is the dataset old's directory
     # the third one is the directory in which we save all the files that contains only the differences
-    def __init__(self,directory_new:str,directory_old:str,directory_diff:str) -> None:
+    def __init__(self,directory_new:str,directory_old:str,directory_diff:str,directory_stats:str) -> None:
         self.directory_new = directory_new
         self.directory_old = directory_old
         self.directory_diff = directory_diff
+        self.directory_stats = directory_stats
         self.total_diff_time = []
-        self.misuration_cpu_usage = []
-        self.misuration_ram_usage = []
 
 
     # function that ciclate on entire dataset that contains a list of file new and the corresponding file old, so, that function, for each file_new, search the corresponding file old
     # (the file old with the same name) and invoke diff to make difference between those two files. This operation is made for all the file new in the dataset and the differences are saved into
     # the diff_folder
     def starting_diff(self):
+        thread1 = Thread(target=self.cpu_usage)
+        thread2 = Thread(target=self.ram_usage)
+        thread1.start()
+        thread2.start()
         directory_new_path = Path(self.directory_new)
         directory_old_path = Path(self.directory_old)
         list_file_new = list(directory_new_path.iterdir())
         list_file_old = list(directory_old_path.iterdir())
         old_names = [f.name for f in list_file_old]
         for file_new in tqdm(list_file_new):
-            if 'opus' not in file_new.name.lower() and file_new.name not in list(Path(self.directory_diff).iterdir()):  # I'm not considering the 'Opus' files because had problem to solve
+            if 'opus' not in file_new.name.lower() and file_new.name not in list(Path(self.directory_diff).iterdir()):          # I'm not considering the 'Opus' files because had problem to solve
                 print(file_new)
                 file_old = None
                 diff_file = str(Path(self.directory_diff) / file_new.name)
@@ -50,18 +54,45 @@ class DatasetDiff:
                 self.total_diff_time.append(diff_time)
                 print(list_file_new.index(file_new))
                 print('\n')
+        thread1.join()
+        thread2.join()
         self.print_stats()
         return
     
 
+    # Function that print into a file all the diff's stats, so we can misure how much efficent our diff is
     def print_stats(self):
+        file_stats = str(Path(self.directory_stats) / "final_stats.txt")
         directory_new_path_size = Path(self.directory_new).stat().st_size
         directory_old_path_size = Path(self.directory_old).stat().st_size
         directory_diff_path_size = Path(self.directory_diff).stat().st_size
-        print('Dataset new size: ', directory_new_path_size, 'bytes', '\n')
-        print('Dataset old size: ', directory_old_path_size, 'bytes', '\n')
-        print('Dataset diff size: ', directory_diff_path_size, 'bytes', '\n')
-        print('Singles diff time: ', self.total_diff_time, '\n')
-        print('Average diff time: ', mean(self.total_diff_time), 'seconds','\n')
-        print('Maximum diff time: ', max(self.total_diff_time), 'seconds', '\n')
+        list_stats = []
+        list_stats.append('Dataset new size: ' + str(directory_new_path_size) + ' bytes')
+        list_stats.append('Dataset old size: ' + str(directory_old_path_size) + 'bytes')
+        list_stats.append('Dataset diff size: ' + str(directory_diff_path_size) + 'bytes')
+        list_stats.append('Average diff time: ' + str(mean(self.total_diff_time)) + 'seconds')
+        list_stats.append('Maximum diff time: ' + str(max(self.total_diff_time)) + 'seconds')
+        with open(file_stats, 'a+') as f:
+            f.writelines(list_stats)
+        return
+    
+
+    # Function that print into a file the CPU usage during the diff execution
+    def cpu_usage(self):
+        file_cpu_stats = str(Path(self.directory_stats) / "CPU_stats.txt")
+        while(True):
+            misuration_cpu_usage = str(psutil.cpu_percent())
+            with open(file_cpu_stats, 'a+') as f:
+                f.write('CPU usage: ' + misuration_cpu_usage+ "\n")
+            time.sleep(10)
+        return
+
+    # Function that print into a file the RAM usage during the diff execution  
+    def ram_usage(self):
+        file_ram_stats = str(Path(self.directory_stats) / "RAM_stats.txt")
+        while(True):
+            misuration_ram_usage = str(psutil.virtual_memory()[2])
+            with open(file_ram_stats, 'a+') as f:
+                f.write('RAM usage: ' + misuration_ram_usage + "\n")
+            time.sleep(10)
         return
--- test_memory_diff/test_diff.py
+++ test_memory_diff/test_diff.py
@@ -6,15 +6,15 @@ import psutil
 
 class Test_TestIncrementDecrement(unittest.TestCase):
 
-    def test_saving_diff(self,directory_new:str ='/home/antonio-lanza/dataset/2022.3.0/en__sw',directory_old:str='/home/antonio-lanza/dataset/2022.2.0/en__sw',directory_diff:str='/home/antonio-lanza/dataset/diff_folder'):
-        dataset_diff_object = DatasetDiff(directory_new,directory_old,directory_diff)
+    def test_saving_diff(self,directory_new:str ='/home/antonio-lanza/dataset/2022.3.0/en__sw',directory_old:str='/home/antonio-lanza/dataset/2022.2.0/en__sw',directory_diff:str='/home/antonio-lanza/dataset/diff_folder',directory_stats:str='/home/antonio-lanza/dataset/stats'):
+        dataset_diff_object = DatasetDiff(directory_new,directory_old,directory_diff,directory_stats)
         dataset_diff_object.starting_diff()
         return
 
-    def test_diff_on_singles_files(self,file_new:str = '/home/antonio-lanza/dataset/2022.3.0/en__sw/MyMemory.46e57154c706da58319f.en-US__sw-SZ.tmx',file_old:str = '/home/antonio-lanza/dataset/2022.2.0/en__sw/MyMemory.46e57154c706da58319f.en-US__sw-SZ.tmx',file_diff:str ='/home/antonio-lanza/dataset/file_diff.tmx'):
-        diff_object = Diff(file_new,file_old,file_diff)
-        diff_object.diff_function()
-        return
+    # def test_diff_on_singles_files(self,file_new:str = '/home/antonio-lanza/dataset/2022.3.0/en__sw/MyMemory.46e57154c706da58319f.en-US__sw-SZ.tmx',file_old:str = '/home/antonio-lanza/dataset/2022.2.0/en__sw/MyMemory.46e57154c706da58319f.en-US__sw-SZ.tmx',file_diff:str ='/home/antonio-lanza/dataset/file_diff.tmx'):
+    #     diff_object = Diff(file_new,file_old,file_diff)
+    #     diff_object.diff_function()
+    #     return
 
 if __name__ == '__main__':
     unittest.main()
