47ffcadc9f7ab5f745fb7fd5c7b1ccd899d1c278
AntonioSouls <tonyplanza1@gmail.com>
2023-03-03 11:24:23 +0000

    Improved algorithm

--- memory_diff/diff.py
+++ memory_diff/diff.py
@@ -5,6 +5,7 @@ from memory_diff.components.tu import TranslatedUnit
 from translate.storage.tmx import tmxfile, tmxunit
 from typing import List
 from xml.etree.ElementTree import fromstring
+import time
 
 # Diff class that models an object that allows you to make the difference between two files and save the difference to another file
 
@@ -30,19 +31,32 @@ class Diff:
         new_list_tu_object = self.opened_file_new.getunits()                          # Saving all tag <tu> (with all their descendants) into a list
         old_list_tu_object = self.opened_file_old.getunits()
 
-        self.diff_function(new_list_tu_object,old_list_tu_object)                   # Invoking the function that litterally parse both <tu> tag's lists and save into the third one only the differences
+        new_dictionary_tu_object =multiprocessing.Manager().dict()
+        old_dictionary_tu_object =multiprocessing.Manager().dict()
+
+
+        for tu_object_new in new_list_tu_object:
+            TranslationUnit_object_new = TranslatedUnit(fromstring(str(tu_object_new)))
+            new_dictionary_tu_object[TranslationUnit_object_new.getId()] = TranslationUnit_object_new
+
+        for tu_object_old in old_list_tu_object:
+            TranslationUnit_object_old = TranslatedUnit(fromstring(str(tu_object_old)))
+            old_dictionary_tu_object[TranslationUnit_object_old.getId()] = TranslationUnit_object_old
+        
+        self.diff_function(new_dictionary_tu_object,old_dictionary_tu_object)                   # Invoking the function that litterally parse both <tu> tag's lists and save into the third one only the differences
 
     
     # Doing diff between the elements of two lists: a list that contains old tag <tu> and a list that contains new tag <tu> and saving into a third list only the differences between the preavious files
-    def diff_function(self, new_list_tu_object,old_list_tu_object):
+    def diff_function(self, new_dictionary_tu_object,old_dictionary_tu_object):
         pool = multiprocessing.Pool(16)                                                                                                             # Creating a pool of 16 processes to parallelize the diff and make 16 diff at a time
         list_block_diff=[]
         list_processes=list()
-        for tu_object_new in new_list_tu_object:                                                                                                                           # I'm parsing the whole list containing the new <tu> blocks and I compare each of these blocks with 
-            list_processes.append(pool.apply_async(Diff.view_added_modified_blocks,args=(str(tu_object_new),[str(u) for u in old_list_tu_object])))                        # all the blocks in the old list invoking the view_added_modified_blocks (I made 16 diff at time with the pool)
+        for new_key in new_dictionary_tu_object.keys():                                                                                                                         # I'm parsing the whole list containing the new <tu> blocks and I compare each of these blocks with 
+            list_processes.append(pool.apply_async(Diff.view_added_modified_blocks,args=(new_key,new_dictionary_tu_object[new_key],old_dictionary_tu_object)))                        # all the blocks in the old list invoking the view_added_modified_blocks (I made 16 diff at time with the pool)
 
-        for tu_object_old in old_list_tu_object:
-            list_processes.append(pool.apply_async(Diff.view_removed_blocks, args=(str(tu_object_old), [str(u) for u in new_list_tu_object])))       #str(tu_object_old),[str(u) for u in new_list_tu_object]
+
+        for old_key in old_dictionary_tu_object.keys():
+            list_processes.append(pool.apply_async(Diff.view_removed_blocks, args=(old_key,new_dictionary_tu_object,old_dictionary_tu_object[old_key])))
             
         
         pool.close()
@@ -59,38 +73,36 @@ class Diff:
    # of this block, it means that this block has been modified so I have to add it to the list that contains differences. I don't have to add anythink to the list that contains differences only in the case of
    # two block that I'm comparing are perfectly equals
     @staticmethod
-    def view_added_modified_blocks(tu_object_new,old_list_tu_object):
-        tu_object_new = TranslatedUnit(fromstring(tu_object_new))
-        exist:bool = False 
-        for i in range(len(old_list_tu_object)):                             
-            if exist:
-                break
-            tu_object_old = TranslatedUnit(fromstring(old_list_tu_object[i]))
-            if tu_object_new.__eq__(tu_object_old):
-                exist = True
+    def view_added_modified_blocks(key,new_tu_object,old_dictionary_tu_object):
         
-        if not exist:
-            return str(tu_object_new)
-        return None
+        start = time.time()
+        print("Inizio thread")
+        if key in old_dictionary_tu_object.keys():
+            if new_tu_object == old_dictionary_tu_object[key]:
+                end = time.time()
+                print(f'{key}: {end - start}')
+                return None
+        tu_object_diff = new_tu_object
+        end = time.time()
+        print(f'{key}: {end - start}')
+        return str(tu_object_diff)
     
 
     # Comparing a single <tu> block old with all the blocks in the new list. This is for seeing if the old <tu> block has been removed in the new list. If in the new list there aren't blocks whith the same id
     # of the <tu> block old, it means that the old <tu> block has been removed, so I have to add it to the list that contains differences but added it as a removed block with the removed tag
     @staticmethod
-    def view_removed_blocks(tu_object_old,new_list_tu_object):
-        tu_object_old = TranslatedUnit(fromstring(tu_object_old))
-        same_id:bool = False
-        for tu_object_new in new_list_tu_object:                                    
-            tu_object_new = TranslatedUnit(fromstring(tu_object_new))
-            if same_id:
-                break
-            if tu_object_old.getId() == tu_object_new.getId():
-                same_id = True
-        if not same_id:
-            object_removed = tu_object_old
-            object_removed.setRemoved(True)
-            return str(object_removed)
-        return None
+    def view_removed_blocks(key,new_dictionary_tu_object,old_tu_object):
+        print("Inizio secondo thread")
+        start = time.time()
+        if key in new_dictionary_tu_object:
+            end = time.time()
+            print(f'{key}: {end - start}')
+            return None
+        tu_object_diff:TranslatedUnit = old_tu_object
+        tu_object_diff.setRemoved(True)
+        end = time.time()
+        print(f'{key}: {end - start}')
+        return str(tu_object_diff)
 
     
     # Saving the differences into a third file called diff_file
